61cb4db744145e364d74c7742f0c2b90f2d3a7f0
AchhyutB <achhyut.baral03@gmail.com>
2026-05-25 18:29:45 +0545

    Hotel Booking Website

--- FrontEnd/src/components/SearchBar.jsx
+++ FrontEnd/src/components/SearchBar.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { assets, cities } from "../assets/assets";
-import { getTodayString } from "../utils/formatDate";
+import { getTodayString } from "../utils/FormatDate";
 
 const SearchBar = ({ initialValues = {} }) => {
   const navigate = useNavigate();
