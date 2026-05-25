098ad5d470023889632dc8ce29f18aeed2bcc7f0
AchhyutB <achhyut.baral03@gmail.com>
2026-05-25 18:32:40 +0545

    Hotel Booking Website

--- FrontEnd/src/components/HotelCard.jsx
+++ FrontEnd/src/components/HotelCard.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { assets } from "../assets/assets";
-import { formatPrice } from "../utils/formatPrice";
+import { formatPrice } from "../utils/FormatPrice";
 
 const HotelCard = ({ room, index }) => {
   const navigate = useNavigate();
